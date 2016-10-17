076031c705358afd13ed3b8e65bf79e89bce0f53
chenshijue <chenshijue@zhixuanhuaxue.com>
2016-10-17 21:01:46 +0800

    change secret to null

--- libs/access-token-task.js
+++ libs/access-token-task.js
@@ -6,7 +6,7 @@ var https = require('https');
 
 var getAccessToken = function (cb){
     https.get("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential" +
-        "&appid=wxbb49660ab88935ae&secret=825d3e03d4628499d18e5dae5f197c32", function (response){
+        "&appid=wxbb49660ab88935ae&secret=SECRET", function (response){
         response.on('data', function (data){
             var _data = JSON.parse(data);
             global.access_token = _data.access_token;
